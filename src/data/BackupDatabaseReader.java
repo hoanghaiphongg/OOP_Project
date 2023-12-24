8acae8d2069039b5bbdcff565c67d21144d1ce34
Hoang Hai Phong <133991823+hoanghaiphongg@users.noreply.github.com>
2023-12-25 03:33:59 +0700

    Update BackupDatabaseReader.java

--- src/data/BackupDatabaseReader.java
+++ src/data/BackupDatabaseReader.java
@@ -1,41 +1,23 @@
-package data;
-import java.util.ArrayList;
-
-import java.util.ArrayList;
-import java.sql.Timestamp;
-//import java.security.Timestamp;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
-import model.Articles;
-import model.BlogPosts;
-import model.Box;
-import model.NFT;
-import model.Tweets;
-
-public class BackupDatabaseReader implements DataReader {
-    private static final String URL = "jdbc:postgresql://localhost:5432/another_database";
-    private static final String USER = "another_username";
-    private static final String PASSWORD = "another_password";
+public class AnotherDatabaseReader implements DataReader {
+    private static final String MYSQL_URL = "jdbc:mysql://localhost:3306/another_database";
+    private static final String MYSQL_USER = "another_username";
+    private static final String MYSQL_PASSWORD = "another_password";
 
     private static Connection connection = null;
 
-    // Implement phương thức từ Interface
     @Override
     public Connection getConnection() throws SQLException, ClassNotFoundException {
         if (connection == null) {
-            Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.print("Connected to another database");
+            Class.forName("com.mysql.cj.jdbc.Driver"); // Use the appropriate MySQL driver class
+            connection = DriverManager.getConnection(MYSQL_URL, MYSQL_USER, MYSQL_PASSWORD);
+            System.out.println("Connected to MySQL database");
         }
         return connection;
     }
 
-    // Các phương thức đọc dữ liệu khác nếu cần
+    // Add other MySQL database operations if needed
 }
