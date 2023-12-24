e90271f8c41e8203fcf04ceed64f3f4aa5c6f2d7
Hoang Hai Phong <133991823+hoanghaiphongg@users.noreply.github.com>
2023-12-24 23:11:22 +0700

    Add files via upload

---
+++ src/model/BlogPosts.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.sql.Date;
+import java.util.List;
+
+public class BlogPosts extends Articles {
+	private String title;
+
+	public BlogPosts(int id, String author, String content, Date timestamp, String link, String title) {
+		super(id, author, content, timestamp, link);
+		this.title = title;
+	}
+
+}
